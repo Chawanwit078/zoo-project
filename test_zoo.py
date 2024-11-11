d2f2b884b5d5d7e79a5ad28694eb6f92f4c8fc59
Chawanwit078 <149600332+Chawanwit078@users.noreply.github.com>
2024-11-11 05:18:58 +0000

    6687078_Lab15

--- test_zoo.py
+++ test_zoo.py
@@ -22,5 +22,48 @@ class TestZoo(unittest.TestCase):
     def test_invalid_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(-5), "Invalid Age")
 
+    def test_invalid_ticket_price_1(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "Invalid Age")
+
+    def test_childe_ticket_price_1(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    
+    def test_childe_ticket_price_2(self):
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
+    
+    def test_childe_ticket_price_3(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+    
+    def test_childe_ticket_price_4(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+    
+    def test_teen_ticket_price_1(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+    
+    def test_teen_ticket_price_2(self):
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+    
+    def test_teen_ticket_price_3(self):
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+    
+    def test_teen_ticket_price_4(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+    
+    def test_adult_ticket_price_1(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    
+    def test_adult_ticket_price_2(self):
+        self.assertEqual(self.zoo.get_ticket_price(22), 150)
+    
+    def test_adult_ticket_price_3(self):
+        self.assertEqual(self.zoo.get_ticket_price(59), 150)
+    
+    def test_adult_ticket_price_4(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)
+    
+    def test_old_ticket_price_1(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+
 if __name__ == '__main__':
     unittest.main()
